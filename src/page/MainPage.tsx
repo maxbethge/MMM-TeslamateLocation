719f1846030248b7555e21ba0cab133265060957
Max Bethge <maxbethge@gmail.com>
2024-07-18 15:10:33 -0700

    updated

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -39,8 +39,8 @@ Module.register<MainPageProps>("MMM-TeslamateLocation", {
         " at port " +
         this.config.mqttServerPort
     );
-    Log.log('[MMM-TeslamateLocation] ' + "1Setting up connection to MQTT Broker " +  this.config.mqttServerAddress + " at port " + this.config.mqttServerPort);
-    this.Log.log('[MMM-TeslamateLocation] ' + "2Setting up connection to MQTT Broker " +  this.config.mqttServerAddress + " at port " + this.config.mqttServerPort);
+    //Log.log('[MMM-TeslamateLocation] ' + "1Setting up connection to MQTT Broker " +  this.config.mqttServerAddress + " at port " + this.config.mqttServerPort);
+    //this.Log.log('[MMM-TeslamateLocation] ' + "2Setting up connection to MQTT Broker " +  this.config.mqttServerAddress + " at port " + this.config.mqttServerPort);
     this.sendSocketNotification("MQTT_CONFIG", this.config);
   },
 
