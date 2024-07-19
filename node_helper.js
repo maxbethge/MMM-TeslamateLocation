b706b6230bafeecc02649c302dc4bde8850d4625
Max Bethge <maxbethge@gmail.com>
2024-07-18 18:37:06 -0700

    updated

--- node_helper.js
+++ node_helper.js
@@ -110,9 +110,9 @@ module.exports = NodeHelper.create({
     });
 
     self.log("before this.sendSocketNotification");
-    this.sendSocketNotification("MQTT_PAYLOAD",{serverKey:"",topic:this.config.mqttTopic,value: 0,time: Date.now(),timeStr:""});
+    this.sendSocketNotification("MQTT_PAYLOAD",{serverKey:"",topic:server.topics[0],value: 0,time: Date.now(),timeStr:""});
     self.log("before self.sendSocketNotification");
-    self.sendSocketNotification("MQTT_PAYLOAD",{serverKey:"",topic:this.config.mqttTopic,value: 0,time: Date.now(),timeStr:""});
+    self.sendSocketNotification("MQTT_PAYLOAD",{serverKey:"",server.topics[0],value: 0,time: Date.now(),timeStr:""});
 
     server.client.on("connect", function (connack) {
       self.log("connected to " + mqttServer);
